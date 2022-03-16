c6d940b64dc84e523f84f3be186a4bca17b89bf8
Aksouda <76596327+Aksouda@users.noreply.github.com>
2022-03-16 20:14:36 +0300

    Update _id.vue

--- pages/result/_id.vue
+++ pages/result/_id.vue
@@ -56,6 +56,12 @@
         {{ domain.score }}  {{ domain.title }}
        </p>
       </div>
+      <script data-b24-form="inline/9/mecp9z" data-skip-moving="true">
+        (function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://bitrix.monefy.ro/upload/crm/form/loader_9_mecp9z.js');
+</script>
       <div
         v-for="domain in results"
         :key="domain.id"
