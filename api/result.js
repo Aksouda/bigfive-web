38dbc9606a1abc7d51f39a4404518cef521d9241
Aksouda <76596327+Aksouda@users.noreply.github.com>
2022-03-18 16:34:01 +0300

    Update result.js

--- api/result.js
+++ api/result.js
@@ -1,8 +1,8 @@
 const { ObjectId } = require('mongodb')
 const validMongoId = require('./lib/valid-mongoid')
 const connectToDb = require('./lib/connect-to-db')
-const getResult = require('@alheimsins/b5-result-text')
-const { getInfo } = require('@alheimsins/b5-result-text')
+const getResult = require('./node_modules/@alheimsins/b5-result-text')
+const { getInfo } = require('./node_modules/@alheimsins/b5-result-text')
 const calculateScore = require('@alheimsins/bigfive-calculate-score')
 
 const { languages: availableLanguages } = getInfo()
