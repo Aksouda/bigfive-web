022828d6b5292fde69a56b0dd1c9c3f308e52e24
Aksouda <76596327+Aksouda@users.noreply.github.com>
2022-04-18 22:04:07 +0300

    unsub

---
+++ pages/unsubscribe.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+   <script data-b24-form="inline/15/0q9w9e" data-skip-moving="true"> (function(w,d,u){ var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0); var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h); })(window,document,'https://bitrix.monefy.ro/upload/crm/form/loader_15_0q9w9e.js'); </script>
+  </div>
+</template>
